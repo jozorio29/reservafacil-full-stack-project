ee8a39e6d858c7cd56e53cd1b20015431fe1d834
jozorio29 <joseozorio29@gmail.com>
2025-06-07 12:39:10 -0300

    Config middlewares

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 
 const proteger = async (req, res, next) => {
+  console.log("🔐 Cookies recibidas:", req.cookies); // agrega esto
   const token = req.cookies.token;
 
   console.log("🔐 Middleware proteger, token recibido:", token);
