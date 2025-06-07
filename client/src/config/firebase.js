f29cc69ee07895cbac3671583cd69e6c185b3ef8
jozorio29 <joseozorio29@gmail.com>
2025-06-07 11:44:46 -0300

    Actualizacion dist

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -1,8 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
