19d6af3b9c6c39694cd70cfe1243c65fd8a59076
jozorio29 <joseozorio29@gmail.com>
2025-06-10 15:59:20 -0300

    server fixed

--- server/server.js
+++ server/server.js
@@ -5,9 +5,7 @@ import dotenv from "dotenv";
 import express from "express";
 import helmet from "helmet";
 import { createServer } from "http";
-import path from "path";
 import { Server } from "socket.io";
-import { fileURLToPath } from "url";
 import { conectarDB, corsOptions } from "./config/config.js";
 import authRoutes from "./routes/auth.routes.js";
 import barberoRoutes from "./routes/barbero.routes.js";
@@ -18,9 +16,6 @@ import userRoutes from "./routes/user.routes.js";
 import "./utils/twilio.js";
 
 dotenv.config();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 const httpServer = createServer(app);
@@ -44,22 +39,6 @@ app.use("/api/reservas", reservationRoutes);
 app.use("/api/usuarios", userRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/barberos", barberoRoutes);
-
-// Esto es necesario si estás usando ESModules (type: "module" en package.json)
-
-// Servir frontend compilado (por ejemplo si usás Vite o React build)
-if (process.env.NODE_ENV === "production") {
-  const buildPath = path.resolve(__dirname, "../client/dist");
-
-console.log("Dirname:", buildPath);
-
-
-  app.use(express.static(buildPath));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(buildPath, "index.html"));
-  });
-}
 
 app.use((req, res, next) => {
   console.log(`🌐 [${req.method}] ${req.originalUrl}`);
