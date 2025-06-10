1e48a653299aa5440c98ca3086517f8a0e45bacb
jozorio29 <joseozorio29@gmail.com>
2025-06-10 16:23:45 -0300

    Config features

--- server/utils/features.js
+++ server/utils/features.js
@@ -1,8 +1,8 @@
 const cookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production", // Solo en producción
-  sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // "None" porque tenemos frontend y backend en dominios distintos con HTTPS
-  // maxAge: 7 * 24 * 60 * 60 * 1000, // 7 días //  Al no definir expires ni maxAge, la cookie se convierte en una cookie de sesión,
+  secure: true, //process.env.NODE_ENV === "production", // Solo en producción
+  sameSite: "None", //process.env.NODE_ENV === "production" ? "None" : "Lax", // "None" porque tenemos frontend y backend en dominios distintos con HTTPS
+  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 días //  Al no definir expires ni maxAge, la cookie se convierte en una cookie de sesión,
 };
 
 export { cookieOptions };
