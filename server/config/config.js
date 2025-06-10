b55cda1f1f6aeb7eaf1b6972cfef3ac9bd928af0
jozorio29 <joseozorio29@gmail.com>
2025-06-10 20:51:35 -0300

    Config server

--- server/config/config.js
+++ server/config/config.js
@@ -24,6 +24,7 @@ const corsOptions = {
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
+  exposedHeaders: ["set-cookie"],
 };
 
 export { conectarDB, corsOptions };
