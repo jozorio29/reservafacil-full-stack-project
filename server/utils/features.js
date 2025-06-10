3b2b31ca52e6110194347ef3344cde9cebbe8072
jozorio29 <joseozorio29@gmail.com>
2025-06-10 16:52:21 -0300

    Config cookieOptions

--- server/utils/features.js
+++ server/utils/features.js
@@ -1,8 +1,9 @@
 const cookieOptions = {
   httpOnly: true,
   secure: true, //process.env.NODE_ENV === "production", // Solo en producción
-  sameSite: "None", //process.env.NODE_ENV === "production" ? "None" : "Lax", // "None" porque tenemos frontend y backend en dominios distintos con HTTPS
+  SameSite: "None", //process.env.NODE_ENV === "production" ? "None" : "Lax", // "None" porque tenemos frontend y backend en dominios distintos con HTTPS
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 días //  Al no definir expires ni maxAge, la cookie se convierte en una cookie de sesión,
+  domain: process.env.COOKIE_DOMAIN,
 };
 
 export { cookieOptions };
